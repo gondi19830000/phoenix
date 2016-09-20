58d58f0f90c424449ee452922b08370057911682
shanglw <shanglw@10.211.55.12>
2016-09-20 22:50:17 +0800

    增加LoggerConstant

---
+++ src/com/shangliwei/phoenix/constant/LoggerConstant.java
@@ -0,0 +1,13 @@
+package com.shangliwei.phoenix.constant;
+
+public final class LoggerConstant {
+
+	public static final String LOGGER_MODE = "2";
+	public static final String LOGGER_MODE_PRODUCT = "1";
+	public static final String LOGGER_MODE_DEVELOP = "2";
+	
+	public static final String LOGGER_LEVEL = "3";
+	public static final String LOGGER_LEVEL_DEBUG = "1";
+	public static final String LOGGER_LEVEL_ERROR = "2";
+	public static final String LOGGER_LEVEL_FULL = "3";
+}
