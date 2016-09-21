37b97131d58b51761a31d129594ae41161fe994a
shanglw <shanglw@10.211.55.12>
2016-09-21 21:58:25 +0800

    1234

--- test/com/shangliwei/phoenix/dao/DepartmentDaoImplTest.java
+++ test/com/shangliwei/phoenix/dao/DepartmentDaoImplTest.java
@@ -2,23 +2,47 @@ package com.shangliwei.phoenix.dao;
 
 import static org.junit.Assert.*;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.shangliwei.phoenix.dao.impl.DepartmentDaoImpl;
+import com.shangliwei.phoenix.domain.po.DepartmentPo;
+import com.shangliwei.phoenix.util.DBUtil;
+import com.shangliwei.phoenix.util.DateTimeUtil;
+import com.shangliwei.phoenix.util.SequenceUtil;
+
 public class DepartmentDaoImplTest {
 
+	private Connection connection;
+	private IDepartmentDao dao = new DepartmentDaoImpl();
+	
 	@Before
 	public void setUp() throws Exception {
+		connection = DBUtil.getConnection();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		DBUtil.getConnection();
 	}
 
 	@Test
-	public void testAdd() {
-		fail("Not yet implemented");
+	public void testAdd() throws SQLException {
+		DepartmentPo po = new DepartmentPo();
+		po.setId(SequenceUtil.getUUID());
+		po.setName("IT支持部");
+		po.setParentId("185");
+		po.setType("01");
+		po.setState("01");
+		po.setManager("admin");
+		po.setCreateDate(DateTimeUtil.getDate());
+		po.setCreater("admin");
+		po.setCreattime(DateTimeUtil.getTimestamp());
+		dao.add(po, connection);
 	}
 
 	@Test
